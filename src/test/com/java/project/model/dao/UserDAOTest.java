7250cdd51937bafe45a6de38d106a5d1b93f7ea3
romaniukV <romaniukv255@gmail.com>
2018-05-21 20:46:08 +0300

    Finished UserDAO tests.

--- src/test/com/java/project/model/dao/UserDAOTest.java
+++ src/test/com/java/project/model/dao/UserDAOTest.java
@@ -7,53 +7,93 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.Assert.*;
 
 public class UserDAOTest {
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         DBConnection.setTestMode();
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         DBConnection.setApplicationMode();
     }
 
     @Test
-    public void findUserByUsername() {
+    public void findUserByUsername() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        connection.setAutoCommit(false);
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.setConnection(connection);
+
         User expectedUser = TestEntities.getTestUser();
-        UserDAO userDAO = new UserDAO();
-        User actualUser = userDAO.findUserByUsername("romaniukv");
+        User actualUser = userDAO.findUserByUsername(expectedUser.getUsername());
+
         assertEquals(expectedUser, actualUser);
+
+        DBConnection.rollbackAndCloseConnection(connection);
     }
 
     @Test
-    public void create() {
+    public void create() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        connection.setAutoCommit(false);
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.setConnection(connection);
+
         User expectedUser = TestEntities.getTestUser2();
-        UserDAO userDAO = new UserDAO();
         userDAO.create(expectedUser);
+
         User createdUser = userDAO.findByKey(expectedUser.getId());
+
         assertEquals(expectedUser, createdUser);
+
+        DBConnection.rollbackAndCloseConnection(connection);
     }
 
     @Test
-    public void update() {
+    public void update() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        connection.setAutoCommit(false);
+
         UserDAO userDAO = new UserDAO();
+        userDAO.setConnection(connection);
+
         User expectedUser = TestEntities.getTestUser2();
         userDAO.create(expectedUser);
         expectedUser.setEmail("newEmail");
         userDAO.update(expectedUser);
+
         User updatedUser = userDAO.findByKey(expectedUser.getId());
+
         assertEquals(expectedUser, updatedUser);
+
+        DBConnection.rollbackAndCloseConnection(connection);
     }
 
-    @Test
-    public void findByKey() {
-    }
 
     @Test
-    public void deleteByKey() {
+    public void deleteByKey() throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        connection.setAutoCommit(false);
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.setConnection(connection);
+
+        User user = TestEntities.getTestUser2();
+        userDAO.create(user);
+        userDAO.deleteByKey(user.getId());
+        User deletedUser = userDAO.findByKey(user.getId());
+
+        assertNull(deletedUser);
+
+        DBConnection.rollbackAndCloseConnection(connection);
     }
 }
